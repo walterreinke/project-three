a3a344bae09b3498924c2941d73fe24825233d99
walterreinke <waltreinke@gmail.com>
2021-05-03 20:56:33 -0400

    got the intersection working and added some more of the content

--- scripts/intersection.js
+++ scripts/intersection.js
@@ -1,7 +1,7 @@
 // this is say that once 25% of the section enters the screen, the effect will take place
 let options = {
   root: null,
-  threshold: 0.25,
+  threshold: 0.15,
 };
 
 function intersectionHandler(entries, observer) {
@@ -11,8 +11,8 @@ function intersectionHandler(entries, observer) {
       // adds active if it is intersecting
       entry.target.classList.add("active");
 
-      // let color = entry.target.dataset.color;
-      // document.body.style.backgroundColor = color;
+      let color = entry.target.dataset.color;
+      document.body.style.backgroundColor = color;
     } else {
       // removes active if it is no longer intersecting
       entry.target.classList.remove("active");
@@ -24,7 +24,8 @@ let observer = new IntersectionObserver(intersectionHandler, options);
 // let is the same thing as creating a var
 
 // need to get reference to all of the sections
-let sections = document.getElementsByClassName("playerslide");
+let sections = document.querySelectorAll("section");
+// let images = document.querySelectorAll("img");
 
 // loop through all the sections, and tell the observer to observe each one
 // for (let i = 0; i < sections.length; i++) {}, this is the ugly way to do the same thing as below
@@ -32,3 +33,7 @@ let sections = document.getElementsByClassName("playerslide");
 sections.forEach((section) => {
   observer.observe(section);
 });
+
+// images.forEach((image) => {
+//   observer.observe(image);
+// });

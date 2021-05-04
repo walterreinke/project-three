fea3242da223b8eaf5ddfed0d7151e80b1491531
walterreinke <waltreinke@gmail.com>
2021-05-04 16:30:46 -0400

    attempting to improve the scroll function by making it scroll to the different sections

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -17,8 +17,22 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  var element = document.getElementById("luka");
+  element.scrollIntoView(true);
 }
+
+// When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//   document.body.scrollTop = 0; // For Safari
+//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// }
+
+// function topFunction() {
+//   window.scrollBy(0, 812);
+// }
+
+// var myElement = document.getElementById("luka");
+// var topPos = myElement.offsetTop;
+
+// document.getElementById("section1").scrolltop = topPos;

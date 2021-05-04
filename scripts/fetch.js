34b7743cae553fd1144c70976e95092a18e01aa2
walterreinke <waltreinke@gmail.com>
2021-05-04 11:09:36 -0400

    made the dropdown menu for the sources, changed the background images, and finalized the content

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -1,28 +1,28 @@
 // this is the actual code that I was working on
-// setup();
+setup();
 
-// function setup() {
-//   getStats();
-// }
+function setup() {
+  getStats();
+}
 
-// function getStats() {
-//   fetch(
-//     "https://www.balldontlie.io/api/v1/stats?seasons[]=2018&seasons[]=2015&player_ids[]=1&player_ids[]=2&postseason=true"
-//   )
-//     .then((response) => response.json())
-//     .then((data) => updateStats(data.data));
-// }
+function getStats() {
+  fetch(
+    "https://www.balldontlie.io/api/v1/stats?seasons[]=2018&seasons[]=2015&player_ids[]=1&player_ids[]=2&postseason=true"
+  )
+    .then((response) => response.json())
+    .then((data) => updateStats(data.data));
+}
 
-// function getFirstName(first_name) {
-//   fetch(first_name)
-//     .then((response) => response.json())
-//     .then((data) => showFirstName(data));
-// }
+function getFirstName(first_name) {
+  fetch(first_name)
+    .then((response) => response.json())
+    .then((data) => showFirstName(data));
+}
 
-// function updateStats(innerText) {
-//   let p = document.getElementById("luka");
-//   p.innerText = innerText;
-// }
+function updateStats(innerText) {
+  let p = document.getElementById("luka");
+  p.innerText = innerText;
+}
 
 // trying to figure out how to work the api with the players one
 // function getStats() {
--- scripts/intersection.js
+++ scripts/intersection.js
@@ -33,17 +33,3 @@ let sections = document.querySelectorAll("section");
 sections.forEach((section) => {
   observer.observe(section);
 });
-
-function disableScroll() {
-  // Get the current page scroll position
-  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  (scrollLeft = window.pageXOffset || document.documentElement.scrollLeft),
-    // if any scroll is attempted, set this to the previous value
-    (window.onscroll = function () {
-      window.scrollTo(scrollLeft, scrollTop);
-    });
-}
-
-function enableScroll() {
-  window.onscroll = function () {};
-}

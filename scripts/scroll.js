9c875bed1cc631b3041726f3b725b01a378f2c9c
walterreinke <waltreinke@gmail.com>
2021-05-04 20:01:59 -0400

    fixed the issue w/ the buttons and finalized the project

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,6 +1,6 @@
 //Get the button:
-mybutton = document.getElementById("myBtn1");
 mybutton = document.getElementById("myBtn2");
+mybutton2 = document.getElementById("myBtn1");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
@@ -13,20 +13,22 @@ function scrollFunction() {
     document.documentElement.scrollTop > 600
   ) {
     mybutton.style.display = "block";
+    mybutton2.style.display = "block";
   } else {
     mybutton.style.display = "none";
+    mybutton2.style.display = "none";
   }
 }
 
 function next() {
   // get the slide with the active class
-  //   document.querySelector(".active").nextSibling.scrollIntoView(true);
-
-  var element = document.querySelector(".active").nextSibling;
-  element.scrollIntoView();
+  let slide = document.querySelector(".active");
+  console.log(slide);
+  console.log(slide.nextElementSibling);
+  document.querySelector(".active").nextElementSibling.scrollIntoView(true);
 }
 
 function previous() {
   // get the slide with the active class
-  document.querySelector(".active").previousSibling.scrollIntoView(true);
+  document.querySelector(".active").previousElementSibling.scrollIntoView(true);
 }
